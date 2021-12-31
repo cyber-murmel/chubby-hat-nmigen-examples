7d57cddfa22acdcbeb6dbb74934cee7a2fb39e51
cyber-murmel <marble@chaoscomputer.club>
2021-12-31 04:37:33 +0100

    cover blink frequency checking

--- blink/blink.py
+++ blink/blink.py
@@ -1,6 +1,7 @@
 from nmigen import *
 from nmigen.asserts import *
 
+
 class Top(Elaboratable):
     def __init__(self, clk_frequency, period_s):
         period = clk_frequency * period_s
@@ -15,63 +16,79 @@ class Top(Elaboratable):
 
         m.submodules.blinker = self.blinker
 
-        if(platform):
+        if platform:
             ledg_n = platform.request("led")
-            m.d.comb += [
-                ledg_n.eq(self.blinker.blink_out)
-            ]
+            m.d.comb += [ledg_n.eq(self.blinker.blink_out)]
 
         return m
 
+
 class Blinker(Elaboratable):
     def __init__(self, period=2):
-        self.half_period = int(period / 2) - 1 # one extra cycle is need for reloading the value
+        self.half_period = int(period / 2)
         self.blink_out = Signal(1)
-        self.counter = Signal(range(self.half_period + 1))
+        self.counter = Signal(range(self.half_period))
 
     @property
     def ports(self):
-        return (self.blink_out, self.counter, )
+        return (
+            self.blink_out,
+            self.counter,
+        )
 
     def elaborate(self, platform):
         m = Module()
 
         # Formal Description Of Behavior
         if "formal" == platform:
+            # use register to count clock cycles
+            ticks = Signal(32)
+            m.d.sync += [
+                ticks.eq(ticks + 1),
+            ]
+
+            def cover_on_and_off():
+                return [
+                    Cover(self.blink_out == 1),
+                    Cover(self.blink_out == 0),
+                ]
+
             m.d.comb += [
                 # assert counter stays in range
                 Assert(self.counter <= self.half_period),
-                # cover being on and off
-                Cover(self.blink_out == 1),
-                Cover(self.blink_out == 0),
-            ]
-            # ignore the first cycle
-            with m.If(~Past(ResetSignal())):
+            ] + cover_on_and_off()
+
+            # let run for one blink cycle
+            with m.If(ticks > 2 * self.half_period):
+                # check that blink frequency is correct
+                m.d.comb += [
+                    Assert(self.blink_out == ~Past(self.blink_out, self.half_period)),
+                    Assert(
+                        self.blink_out == Past(self.blink_out, 2 * self.half_period)
+                    ),
+                ] + cover_on_and_off()
+
                 # counter reaches 0
                 with m.If(Past(self.counter) == 0):
                     m.d.sync += [
                         # assert counter gets reset and LED flips
-                        Assert(self.counter == self.half_period),
+                        Assert(self.counter == self.half_period - 1),
                         Assert(self.blink_out != Past(self.blink_out)),
-                        # cover turning on and off
-                        Cover(self.blink_out == 1),
-                        Cover(self.blink_out == 0),
-                    ]
+                    ] + cover_on_and_off()
                 with m.Else():
                     m.d.comb += [
                         # assert that counter is counting and LED keeps state
-                        Assert(self.counter == Past(self.counter)-1),
+                        Assert(self.counter == Past(self.counter) - 1),
                         Assert(self.blink_out == Past(self.blink_out)),
-                        # cover being on and off
-                        Cover(self.blink_out == 1),
-                        Cover(self.blink_out == 0),
-                    ]
+                    ] + cover_on_and_off()
 
         # Definition of Behavior
         with m.If(self.counter == 0):
             m.d.sync += [
                 self.blink_out.eq(~self.blink_out),
-                self.counter.eq(self.half_period)
+                self.counter.eq(
+                    self.half_period - 1
+                ),  # one extra cycle is need for reloading the value
             ]
         with m.Else():
             m.d.sync += self.counter.eq(self.counter - 1)
